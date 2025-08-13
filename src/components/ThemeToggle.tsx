06a941b86412735bfd799186b8577fb4314ee5c1
brian054 <brian.rowell54@gmail.com>
2025-08-13 17:53:49 -0500

    fix the mobile version of the dark/light button later, hiding it on mobile for now

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -37,7 +37,7 @@ export const ThemeToggle = () => {
     <button
       onClick={toggleTheme}
       className={cn(
-        "fixed top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+        "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
         "focus:outline-none"
       )}
     >
